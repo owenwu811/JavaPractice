97ac7f82250968670a3d9295da3e060a2e3ebcb5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-10-30 22:32:22 -0400

    Create constructors.java

---
+++ constructors.java
@@ -0,0 +1,10 @@
+public class constructors {
+    int x;
+    public constructors() {
+        x = 5;
+    }
+    public static void main(String[] args) {
+        constructors c = new constructors();
+        System.out.println(c.x);
+    }
+}
